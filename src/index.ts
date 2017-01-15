7f3d49312e6037234d9b82730995dba74d0a2337
Dan Gebhardt <dan@cerebris.com>
2017-01-15 17:53:49 -0500

    Introduce specifier interface and utilities.

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,10 @@ export { Factory } from './factory';
 export { default as Registry, Injection, RegistrationOptions } from './registry';
 export { Resolver } from './resolver';
 export { Owner, getOwner, setOwner, OWNER } from './owner';
+export {
+  Specifier,
+  isSpecifierStringAbsolute,
+  isSpecifierObjectAbsolute,
+  serializeSpecifier,
+  deserializeSpecifier
+} from './specifier';
